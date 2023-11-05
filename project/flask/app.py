4203db7524f7645767a16b63fc9c1332153b09dc
bee <bee85919@gmail.com>
2023-11-05 19:43:05 +0900

    Create app.py

---
+++ project/flask/app.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask
+from flask_cors import CORS
+from Routes import total, auth
+
+
+app = Flask(__name__)
+app.register_blueprint(total.app)
+app.register_blueprint(auth.app)
+
+
+# 외부 api호출 허용
+print(os.environ.get('FRONTEND_HOST'))
+CORS(app, resources={r"//*": {"origins": "*"}})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='8000')
